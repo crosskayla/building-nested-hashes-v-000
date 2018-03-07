2ca30cbb157f9fff0e4ab27a6eec90e25edcb6a1
Kayla Cross <kac288@georgetown.edu>
2018-03-07 02:29:57 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,21 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
+      },
+      :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
+      },
+      :hero => {
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
+      },
       :hero_friends => []
-      }, 
+      },
     :capulet => {
       :patriarch => {},
       :matriarch => {},
@@ -14,5 +24,5 @@ def second_challenge
       }
     }
 
-  
+
 end
